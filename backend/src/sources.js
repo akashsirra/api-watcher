73170589aa1bd93128ce94c1b5e7cde5a5f53a54
akashsirra <akashsirra24@gmail.com>
2026-09-18 10:59:50 +0530

    Add initial API sources

---
+++ backend/src/sources.js
@@ -0,0 +1,6 @@
+export const SOURCES=[
+ {slug:"openai",name:"OpenAI",url:"https://platform.openai.com/docs/changelog"},
+ {slug:"stripe",name:"Stripe",url:"https://docs.stripe.com/changelog"},
+ {slug:"github",name:"GitHub",url:"https://github.blog/changelog/"},
+ {slug:"twilio",name:"Twilio",url:"https://www.twilio.com/en-us/changelog"}
+];
